afd776a5377e26b3ef97b90195e22b05de1b7461
Olena Falieieva <elena.faleeva@gmail.com>
2015-05-19 22:51:36 +0300

    For review

--- Servlets/src/test/java/ua/goit/servlets/servlets/TestCategoriesServlet.java
+++ Servlets/src/test/java/ua/goit/servlets/servlets/TestCategoriesServlet.java
@@ -2,40 +2,41 @@ package ua.goit.servlets.servlets;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class TestCategoriesServlet extends Mockito {
-	private CategoriesServlet servlet;
+  private CategoriesServlet servlet;
 
-	@Before
-	public void setUp() throws ServletException {
-		servlet = new CategoriesServlet();
-		servlet.init();
-	}
-	
-	@Test
-	public void testServlet() throws Exception {
-		HttpServletRequest request = mock(HttpServletRequest.class);       
-		HttpServletResponse response = mock(HttpServletResponse.class); 
-		when(request.getRequestURI()).thenReturn("http://localhost:8080/Servlets/categories");
-		PrintWriter writer = new PrintWriter("fileServletCategories.txt");
-		when(response.getWriter()).thenReturn(writer);
+  @Before
+  public void setUp() throws ServletException {
+	servlet = new CategoriesServlet();
+	servlet.init();
+  }
 
-		servlet.doGet(request, response);
-		writer.flush();
-		
-		assertTrue(FileUtils.readFileToString(new File("fileServletCategories.txt"))
-				.contains("<a href=/Servlets/projects?category=IT>IT</a><br>"
-						+ "<a href=/Servlets/projects?category=ART>ART</a><br>"));
-	}
+  @Test
+  public void testServlet() throws Exception {
+	HttpServletRequest request = mock(HttpServletRequest.class);       
+	HttpServletResponse response = mock(HttpServletResponse.class); 
+	when(request.getRequestURI()).thenReturn("http://localhost:8080/Servlets/categories");
+
+	StringWriter stringWriter = new StringWriter();
+	PrintWriter writer = new PrintWriter(stringWriter);
+	when(response.getWriter()).thenReturn(writer);
+
+	servlet.doGet(request, response);
+	String result = stringWriter.toString();
+	writer.flush();
+
+	assertEquals("<a href=/Servlets/projects?category=IT>IT</a><br>"
+		+ "<a href=/Servlets/projects?category=ART>ART</a><br>", result);
+  }
 }
--- Servlets/src/test/java/ua/goit/servlets/servlets/TestProjectsServlet.java
+++ Servlets/src/test/java/ua/goit/servlets/servlets/TestProjectsServlet.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -13,39 +12,33 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.FileUtils;
 import org.mockito.Mockito;
 
 public class TestProjectsServlet extends Mockito {
-	 ProjectsServlet servlet;
+  ProjectsServlet servlet;
 
-	@Before
-	public void setUp() throws ServletException {
-		servlet = new ProjectsServlet();
-		servlet.init();
-	}
+  @Before
+  public void setUp() throws ServletException {
+	servlet = new ProjectsServlet();
+	servlet.init();
+  }
 
-	@Test
-	public void testServlet() throws Exception {
-		HttpServletRequest request = mock(HttpServletRequest.class);       
-		HttpServletResponse response = mock(HttpServletResponse.class); 
-		when(request.getRequestURI()).thenReturn("http://localhost:8080/Servlets/projects?category=ART");
-		when(request.getParameter("category")).thenReturn("ART");
-		//PrintWriter writer = new PrintWriter("fileServletProjects.txt");
-		
-		StringWriter stringWriter = new StringWriter();
-		PrintWriter writer = new PrintWriter(stringWriter);
-		when(response.getWriter()).thenReturn(writer);
-		String result = stringWriter.toString();
-//		StringWriter stringWriterExp = new StringWriter();
-//		stringWriterExp.write("<ul><li> Art.1 </li><li> Art.2 </li><li> Art.3 </li></ul>");
-//		String res = stringWriterExp.
-		
-		servlet.doGet(request, response);
-		verify(request, atLeast(1)).getParameter("category");
-		writer.flush();
-//		assertTrue(FileUtils.readFileToString(new File("fileServletProjects.txt"))
-//				.contains("<ul><li> Art.1 </li><li> Art.2 </li><li> Art.3 </li></ul>"));
-		assertEquals("<ul><li> Art.1 </li><li> Art.2 </li><li> Art.3 </li></ul>", result);
-	}
+  @Test
+  public void testServlet() throws Exception {
+	HttpServletRequest request = mock(HttpServletRequest.class);       
+	HttpServletResponse response = mock(HttpServletResponse.class); 
+	when(request.getRequestURI()).thenReturn("http://localhost:8080/Servlets/projects?category=ART");
+	when(request.getParameter("category")).thenReturn("ART");
+
+	StringWriter stringWriter = new StringWriter();
+	PrintWriter writer = new PrintWriter(stringWriter);
+	when(response.getWriter()).thenReturn(writer);
+
+	servlet.doGet(request, response);
+	verify(request, atLeast(1)).getParameter("category");
+	String result = stringWriter.toString();
+	writer.flush();
+
+	assertEquals("<ul><li> Art.1 </li><li> Art.2 </li><li> Art.3 </li></ul>", result);
+  }
 }
