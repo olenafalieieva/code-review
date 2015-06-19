ed0910ed2f163c9937347e50e3f1bfa42940fcd0
Olena Falieieva <elena.faleeva@gmail.com>
2015-06-19 14:26:32 +0300

    fixed

--- RotatedArray/src/ua/goit/alg/RotatedArray.java
+++ RotatedArray/src/ua/goit/alg/RotatedArray.java
@@ -5,24 +5,29 @@ public class RotatedArray {
     public static int searchInRotatedArray(int array[], int length, int value) {
 	int left = 0;
 	int right = length - 1;
-
+	if ((length == 1) && (value == array[length - 1])) {
+	    return 0;
+	}
 	while (left < right) {
 	    int middle = left + right / 2;
-	    if (array[middle] == value) 
+	    if (array[middle] == value) {
 		return middle;
+	    }	    
 	    if (array[left] <= array[middle]) {
-		if (array[left] <= value && value < array[middle])
+		if (array[left] <= value && value < array[middle]) {
 		    right = middle - 1;
-		else
+		} else {
 		    left = middle + 1;
-	    }
-	    else {
-		if (array[middle] < value && value <= array[right])
+		}
+	    } else {
+		if (array[middle] < value && value <= array[right]) {
 		    left = middle + 1;
-		else 
+		} else {
 		    right = middle - 1;
+		}
 	    }
 	}
+
 	return -1;
     }
 }
--- RotatedArray/src/ua/goit/alg/TestRotatedArray.java
+++ RotatedArray/src/ua/goit/alg/TestRotatedArray.java
@@ -20,4 +20,28 @@ public class TestRotatedArray{
 	int expectedIndex = -1;
 	Assert.assertEquals(expectedIndex, actualIndex);
     }
+    
+    @Test
+    public void shouldReturnResultForEmtyArr() {
+	int Array[] = {};
+	int actualIndex = RotatedArray.searchInRotatedArray(Array, 0, 0);
+	int expectedIndex = -1;
+	Assert.assertEquals(expectedIndex, actualIndex);
+    }
+    
+    @Test
+    public void shouldReturnResultForOneElementArr() {
+	int Array[] = {100500};
+	int actualIndex = RotatedArray.searchInRotatedArray(Array, 1, 100500);
+	int expectedIndex = 0;
+	Assert.assertEquals(expectedIndex, actualIndex);
+    }
+    
+       @Test
+    public void shouldReturnResultForTwoElementArr() {
+	int Array[] = {100500, 8};
+	int actualIndex = RotatedArray.searchInRotatedArray(Array, 2, 100500);
+	int expectedIndex = 0;
+	Assert.assertEquals(expectedIndex, actualIndex);
+    }
 }
