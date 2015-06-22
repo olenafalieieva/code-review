2ff2c822ef5a735f422701a3e97efbdb66c2c999
Olena Falieieva <elena.faleeva@gmail.com>
2015-06-22 17:59:55 +0300

    fixed

--- RotatedArray/src/ua/goit/alg/TestRotatedArray.java
+++ RotatedArray/src/ua/goit/alg/TestRotatedArray.java
@@ -7,40 +7,40 @@ public class TestRotatedArray{
 
     @Test
     public void shouldReturnSearchResult() {
-	int Array[] = {5, 6, 7, 8, 1, 2, 3, 4};
-	int actualIndex = RotatedArray.searchInRotatedArray(Array, 8, 8);
+	int array[] = {5, 6, 7, 8, 1, 2, 3, 4};
+	int actualIndex = RotatedArray.searchInRotatedArray(array, 8, 8);
 	int expectedIndex = 3;
 	Assert.assertEquals(expectedIndex, actualIndex);
     }
-    
+
     @Test
     public void shouldNotReturnSearchResult() {
-	int Array[] = {5, 6, 7, 8, 1, 2, 3, 4};
-	int actualIndex = RotatedArray.searchInRotatedArray(Array, 8, 9);
+	int array[] = {5, 6, 7, 8, 1, 2, 3, 4};
+	int actualIndex = RotatedArray.searchInRotatedArray(array, 8, 9);
 	int expectedIndex = -1;
 	Assert.assertEquals(expectedIndex, actualIndex);
     }
-    
+
     @Test
     public void shouldReturnResultForEmtyArr() {
-	int Array[] = {};
-	int actualIndex = RotatedArray.searchInRotatedArray(Array, 0, 0);
+	int array[] = {};
+	int actualIndex = RotatedArray.searchInRotatedArray(array, 0, 0);
 	int expectedIndex = -1;
 	Assert.assertEquals(expectedIndex, actualIndex);
     }
-    
+
     @Test
     public void shouldReturnResultForOneElementArr() {
-	int Array[] = {100500};
-	int actualIndex = RotatedArray.searchInRotatedArray(Array, 1, 100500);
+	int array[] = {100500};
+	int actualIndex = RotatedArray.searchInRotatedArray(array, 1, 100500);
 	int expectedIndex = 0;
 	Assert.assertEquals(expectedIndex, actualIndex);
     }
-    
-       @Test
+
+    @Test
     public void shouldReturnResultForTwoElementArr() {
-	int Array[] = {100500, 8};
-	int actualIndex = RotatedArray.searchInRotatedArray(Array, 2, 100500);
+	int array[] = {100500, 8};
+	int actualIndex = RotatedArray.searchInRotatedArray(array, 2, 100500);
 	int expectedIndex = 0;
 	Assert.assertEquals(expectedIndex, actualIndex);
     }
