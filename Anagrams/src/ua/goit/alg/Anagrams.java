8018c7e8ecc4d69422558d7cf4eea75e697691aa
Olena Falieieva <elena.faleeva@gmail.com>
2015-06-22 17:07:56 +0300

    fixed

--- Anagrams/src/ua/goit/alg/Anagrams.java
+++ Anagrams/src/ua/goit/alg/Anagrams.java
@@ -4,44 +4,44 @@ import java.util.Scanner;
 
 public class Anagrams {
 
-	public static void anagram(InputStream in) {
-		Scanner sc = new Scanner(in);
-		String input = null;
-		int j = 0;
-		int i = 0;
-		input = sc.nextLine(); 
-		while (i < input.length()) {
-			if (input.charAt(i) == ' ') {
-				j++;
-			}
-			i++;
-		}
-		sc.close();
-		int wordsQ = j+1;
-		j = 0;
-		i = 0;
-		int countChars = 0;
-		int start = 0;
-		while (wordsQ > i) {
-			while ((j < input.length() - 1) && (input.charAt(j) != ' ')) {
-				countChars++;
-				j++;
-			}
-			if ((j == input.length() - 1) && (wordsQ > 1)) {
-				while (countChars < j) {
-					countChars++;
-				}
-			}
-			for (int k = countChars; k >= start; --k) {
-				System.out.print(input.charAt(k));
-			}
-			countChars = countChars + 1;
-			start = countChars;
-			i++;
-			j++;
-			if (wordsQ > i) {
-				System.out.print(" ");
-			}
-		}
-	}
+    public static void anagram(InputStream in) {
+        Scanner sc = new Scanner(in);
+        String input = null;
+        int j = 0;
+        int i = 0;
+        input = sc.nextLine(); 
+        while (i < input.length()) {
+            if (input.charAt(i) == ' ') {
+                j++;
+            }
+            i++;
+        }
+        sc.close();
+        int wordsQ = j+1;
+        j = 0;
+        i = 0;
+        int countChars = 0;
+        int start = 0;
+        while (wordsQ > i) {
+            while ((j < input.length() - 1) && (input.charAt(j) != ' ')) {
+                countChars++;
+                j++;
+            }
+            if ((j == input.length() - 1) && (wordsQ > 1)) {
+                while (countChars < j) {
+                    countChars++;
+                }
+            }
+            for (int k = countChars; k >= start; --k) {
+                System.out.print(input.charAt(k));
+            }
+            countChars = countChars + 1;
+            start = countChars;
+            i++;
+            j++;
+            if (wordsQ > i) {
+                System.out.print(" ");
+            }
+        }
+    }
 }
--- Anagrams/src/ua/goit/alg/TestAnagrams.java
+++ Anagrams/src/ua/goit/alg/TestAnagrams.java
@@ -14,33 +14,33 @@ import org.junit.Test;
 
 public class TestAnagrams {
 
-	@Test
-	public void twoWords() throws UnsupportedEncodingException {
-		String input = "hello world!";
-		String expected = " olleh !dlrow";
-		InputStream stubInputStream = new ByteArrayInputStream(input.getBytes("UTF-8"));
-		String actual = outputToString(stubInputStream);
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void twoWords() throws UnsupportedEncodingException {
+        String input = "hello world!";
+        String expected = " olleh !dlrow";
+        InputStream stubInputStream = new ByteArrayInputStream(input.getBytes("UTF-8"));
+        String actual = outputToString(stubInputStream);
+        assertEquals(expected, actual);
+    }
 
-	@Test
-	public void threeWords() throws UnsupportedEncodingException {
-		String expected = " yM  eman :si";
-		String input = "My name is:";
-		InputStream stubInputStream = new ByteArrayInputStream(input.getBytes("UTF-8"));
-		String actual = outputToString(stubInputStream);
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void threeWords() throws UnsupportedEncodingException {
+        String expected = " yM  eman :si";
+        String input = "My name is:";
+        InputStream stubInputStream = new ByteArrayInputStream(input.getBytes("UTF-8"));
+        String actual = outputToString(stubInputStream);
+        assertEquals(expected, actual);
+    }
 
-	private String outputToString(InputStream stubInputStream) {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos);
-		PrintStream old = System.out;
-		System.setOut(ps);
-		Anagrams.anagram(stubInputStream);
-		System.out.flush();
-		System.setOut(old);
-		String result = baos.toString();
-		return result;
-	}
+    private String outputToString(InputStream stubInputStream) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        PrintStream old = System.out;
+        System.setOut(ps);
+        Anagrams.anagram(stubInputStream);
+        System.out.flush();
+        System.setOut(old);
+        String result = baos.toString();
+        return result;
+    }
 }
